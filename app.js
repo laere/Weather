a13a6efb05abb84f96dc7257f39976eab5082263
Zack <bostianz@gmail.com>
2016-01-14 11:53:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,19 +11,21 @@
 $(function() {
   //API key
   var $apiKey = '34061ce86bfda355e56b98b6d018e56c',
-      $input = $('#inputCity'), //val of input used to url
-      //output when ajax call success
-      $output = $('#output'),
-      //submit button
-      $submitButton = $('#submit'),
-      $message;
+  $input = $('#inputCity'), //val of input used to url
+  //output when ajax call success
+  $output = $('#output'),
+  //submit button
+  $submitButton = $('#submit');
 
+  // on ENTER keypress call AJAX function.
   $input.keypress(function(event) {
     if (event.which === 13) {
       getWeatherData();
+    } else {
+      event.stopPropagation();
     }
   });
-
+  // on click call AJAX function
   $submitButton.click(function() {
     getWeatherData();
   });
@@ -31,7 +33,6 @@ $(function() {
   function convertToCelsius(f) {
     return (f - 32) / 1.8000;
   }
-
   function getWeatherData() {
     $.ajax({
       type: 'GET',
@@ -39,15 +40,21 @@ $(function() {
       url: 'http://api.openweathermap.org/data/2.5/weather?q=' + $input.val() +'&units=imperial&appid=' + $apiKey,
       //on success print weather and temperature
       success: function(data) {
-        var celsius = convertToCelsius(data.main.temp);
+        var celsius = convertToCelsius(data.main.temp),
+            $message,
+            $twitter = $('.twitter');
         console.log('success!');
         console.log(data);
         // var kelvinTemp = data.main.temp;
         // var kelvinToFarenheit = (kelvinTemp - 273.15)*1.8000 + 32;
-          $message = 'Weather: ' + data.weather[0].description + '.';
-          $message += ' Temp: ' + data.main.temp.toFixed(0) + ' degrees Farenheit. ';
-          $message += celsius.toFixed(0) + ' degrees Celsius';
-          $output.html($message);
+        $message = 'Weather: ' + data.weather[0].description + '.';
+        $message += ' Temp: ' + data.main.temp.toFixed(0) + ' degrees Farenheit. ';
+        $message += celsius.toFixed(0) + ' degrees Celsius';
+        $output.html($message);
+
+        $twitter.click(function() {
+          window.open('https://twitter.com/intent/tweet?text=' + $message + ' here in ' + $input.val() + '!');
+        });
       },
       //on error log error message
       error: function() {
@@ -55,5 +62,4 @@ $(function() {
       }
     });
   }
-
 });
